61cd7eabeb3feac2e5561de186a003eb3434eeb0
Sato <sato@schefferlogistica.com.br>
2016-09-10 21:09:14 -0300

    initial

---
+++ Quiron.LojaVirtual.UnitTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Quiron.LojaVirtual.UnitTest
+{
+  [TestClass]
+  public class UnitTestQuiron
+  {
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+  }
+}
